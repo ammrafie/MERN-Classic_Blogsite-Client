74475e0fd733e6a4cfce01716e016808ff1ed2d2
raafee <ammrafie@gmail.com>
2022-07-07 00:27:54 +0600

    Update static file path

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ console.log("############################");
 const app = express();
 
 // After merging front-end, we have to tell where our static files are located.
-app.use(express.static("public"));   // Old version: path.join(__dirname, '/build')
+app.use(express.static("/public"));   // Old version: path.join(__dirname, '/build')
 
 console.log("############################");
 console.log("Done with telling where the static files are!");
@@ -93,7 +93,7 @@ app.post('/api/articles/:name/add-comment', (req,res) => {
 // All requests that aren't caught by any other api routes should be passed onto our app
 // This will allow our client-side app to navigate between pages and process URLs correctly
 app.get('*', (req, res) => {
-    res.sendFile("public/index.html");  // Old version:  path.join(__dirname, '/build/index.html')
+    res.sendFile("/public/index.html");  // Old version:  path.join(__dirname, '/build/index.html')
 })
 
 // Actually Start the server
